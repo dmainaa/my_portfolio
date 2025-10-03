d15b59cabd6a1c403fb76b5a855f1ca7d519d04a
dmainaa <denismaina96.com>
2025-10-03 12:39:46 +0300

    feat: added my skills section

--- lib/presentation/my_skills/my_skills.dart
+++ lib/presentation/my_skills/my_skills.dart
@@ -1,68 +1,79 @@
 import 'package:flutter/material.dart';
-import 'package:my_portifolio/domain/models/skill.dart';
+
 import 'package:portfolio_components/portfolio_components.dart';
 
 class PFSkillsSection extends StatelessWidget {
   final bool isMobile;
-  const PFSkillsSection({super.key, required this.isMobile});
+  PFSkillsSection({super.key, required this.isMobile});
+
+  List<String> get skillCategories => ['Mobile', 'Web', 'AI'];
+
+  List<PFSkill> get mobileSkills => [
+    PFSkill(title: 'Flutter', percentage: 0.8),
+    PFSkill(title: 'Dart', percentage: 0.9),
+    PFSkill(title: 'Java', percentage: 0.7),
+    PFSkill(title: 'Kotlin', percentage: 0.6),
+  ]; 
+  List<PFSkill> get webSkills => [
+    PFSkill(title: 'JavaScript', percentage: 0.7),
+    PFSkill(title: 'TypeScript', percentage: 0.6),
+    PFSkill(title: 'React', percentage: 0.8),
+    PFSkill(title: 'Angular', percentage: 0.5),
+  ];
+  List<PFSkill> get aiSkills => [
+    PFSkill(title: 'Machine Learning', percentage: 0.7),
+    PFSkill(title: 'Data Science', percentage: 0.6),
+    PFSkill(title: 'Python', percentage: 0.8),
+    PFSkill(title: 'TensorFlow', percentage: 0.5),
+    ];
+
+  List<Map<String, List<PFSkill>>>?  skillsByCategory;
 
   @override
   Widget build(BuildContext context) {
     return PFSectionWidget(
       title: "My Skills",
-      child: isMobile ? Column(
-        children: getAllSkills(isMobile),
-      ) : Row(
-        children: getAllSkills(isMobile),
+      child: LayoutBuilder(
+        builder: (context, constraints) =>
+        GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: isMobile ? 1 : 3,
+          ),
+          itemBuilder: (context, index) {
+            final skills = getAllSkills(isMobile, constraints);
+            return skills[index];
+          },
+          itemCount: getAllSkills(isMobile, constraints).length,
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+        ),
       ),
     );
   }
+
+  List<Widget> getAllSkills(bool isMobile, BoxConstraints constraints) {
+ skillsByCategory = [
+      {'Mobile': mobileSkills},
+      {'Web': webSkills},
+      {'AI': aiSkills},
+    ];
+
+    return skillsByCategory!.map((categoryMap) {
+      final category = categoryMap.keys.first;
+      final skills = categoryMap[category] ?? [];
+      return PFSkillsCard(
+        skills: skills,
+        constraints: constraints,
+        title: category,
+      );
+    }).toList();
+}
 }
 
-List<Widget> getAllSkills(bool isMobile) {
-  return [
-    PFSkillWidget(
-            isMobile: isMobile,
-            skills: getMySkills(
-              skills: [Skill(name: 'Flutter', percentage: 0.7)],
-            ),
-            skillCategory: 'Frontend',
-          ),
-  ];
-}
 
-List<Widget> getMySkills({required List<Skill> skills}) {
+
+List<Widget> getMySkills({required List<PFSkill> skills}) {
   return skills
       .map((skill) => PFSkillLevel(percentage: skill.percentage))
       .toList();
-}
-
-class PFSkillWidget extends BaseCard {
-  final List<Widget> skills;
-  final String skillCategory;
-  final bool isMobile;
-  const PFSkillWidget({
-    super.key,
-    required this.isMobile,
-    required this.skills,
-    required this.skillCategory,
-  });
-
-  @override
-  Widget get child => SizedBox(
-    width: isMobile ? double.infinity : 200,
-    child: Column(
-      children: [
-        PFText(
-          skillCategory,
-          style: PFAppTypography.bold.copyWith(
-            color: PFAppColors.accent,
-            fontSize: PFAppSize.s16,
-          ),
-        ),
-        PFSpacer(),
-        ...skills,
-      ],
-    ),
-  );
 }
